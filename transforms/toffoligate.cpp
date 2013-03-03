5ec32d5daae8b4178e2999cc981ce4289f34cd77
Roman Bolshakov <rombolshak@gmail.com>
2013-03-03 23:09:35 +0400

    Some example transformations

---
+++ transforms/toffoligate.cpp
@@ -0,0 +1,40 @@
+/*
+    Copyright (c) 2013 Роман <rombolshak@russia.ru>
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+#include "toffoligate.h"
+
+ToffoliGate::ToffoliGate()
+{
+    Matrix<std::complex< double >, 8, 8> matr;
+    matr.setConstant(0);
+    matr.block<6,6>(0,0).setIdentity();
+    matr(7, 6) = matr(6, 7) = 1;
+    
+    std::vector<uint> dims; for (int i = 0; i < 3; ++i) dims.push_back(2);;
+    _matrix = matr;
+    _space = HilbertSpace(dims);
+}
+
